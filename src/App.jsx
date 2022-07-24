641fbc1ef1a41ff21891f6ab861cc52605b08978
t-kyab <hajsu00@gmail.com>
2022-07-24 22:50:46 +0900

    prepare for redux, material UI and history etc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import Counter from './components/Counter';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [id, setId] = useState('deatiger')
+  const [name, setName] = useState('')
+  const ids = ['deatiger', 'gaearon', 'aws', 'facebook']
+  const getRamdomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)]
+    setId(_id)
+  }
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/${id}')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setName(data.neme)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  }, [id])
   return (
-    // <div>
-    //   <TextInput />
-    // </div>
     <div>
-      <Counter />
+      <p>{id}の、Github上の名前は{name}です。</p>
+      <button onClick={getRamdomId}>IDを変更</button>
     </div>
   );
 }
